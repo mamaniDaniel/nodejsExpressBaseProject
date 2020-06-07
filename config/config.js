802171eda9efe8bf3a108232df9c80ef708a8b96
daniel mamani <dani.oscar.257@gmail.com>
2020-06-07 17:51:34 -0300

    OK: agregado modelo usuario y bcrypt para guardar password

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  SaltWorkFactor: process.env.SALT_WORK_FACTOR || 10, //A: las vueltas que usa bcrypt para encriptar las password
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' +
---
+++ server/models/user.model.js
@@ -0,0 +1,59 @@
+const mongoose= require('mongoose');
+const bcrypt= require('bcrypt');
+const config= require('../../config/config');
+
+const UserSchema= new mongoose.Schema({
+  name:{
+    type:String,
+    trim: true,
+    required: 'Nombre es requerido'
+  },
+  email:{
+    type: String,
+    trim: true,
+    unique: 'El email ya existe',
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    required: 'Email es requerido'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  updated: Date,
+  hashedPassword: {
+    type: String,
+    required: 'Contraseña es requerida'
+  }
+})
+
+
+//INFO: https://stackoverflow.com/a/14595363/10401760
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(config.SaltWorkFactor, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+  });
+};
+
+module.exports= mongoose.model('User', UserSchema);
